7e877cfe2d4ad16d57b6944840b6d9e89bce98d5
Businessm3n <vietinadaniele9@gmail.com>
2024-03-04 15:55:40 +0100

    Sito

--- login.php
+++ login.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login</h2>
-    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <div>
-            <label for="username">Username</label>
-            <input type="text" name="username" id="username">
-        </div>
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password">
-        </div>
-        <div>
-            <button type="submit" name="login">Login</button>
-        </div>
-    </form>
-</body>
-</html>
+
 
 <?php
 // Include il file di configurazione del database
